e6914c29ec6ede480c50aebcb01bd860b40cf448
Daniel Cruz <danielcruzp@unitec.edu>
2023-08-25 13:45:38 -0600

    Avancito

--- src/cmdlab2/CMDLogic.java
+++ src/cmdlab2/CMDLogic.java
@@ -1,8 +1,26 @@
 package cmdlab2;
 
-
+import java.io.File;
+import java.io.IOException;
 
 public class CMDLogic {
+    
+    private File mifile = null;
+
+    void setFile(String direccion) {
+        mifile = new File(direccion);
+    }
+    
+    boolean crearFile() throws IOException {
+        return mifile.createNewFile();
+    }
+
+    boolean crearFolder() {
+        return mifile.mkdirs();
+    }
+    
+    
+    
     
     
     
--- src/cmdlab2/CMDMain.java
+++ src/cmdlab2/CMDMain.java
@@ -4,6 +4,7 @@ public class CMDMain {
     
     public static void main(String[] args) {
         
+        
     }
     
     
